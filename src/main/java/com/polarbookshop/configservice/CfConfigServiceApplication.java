ff82c0f62d1a259fcd6d1f247079a121d1703045
Edgar Solano Rojas <esolano@proveedor.intercorp.com.pe>
2024-09-01 21:35:34 -0500

    FEAT: INITIAL COMMIT

---
+++ src/main/java/com/polarbookshop/configservice/CfConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfConfigServiceApplication.class, args);
+	}
+
+}
